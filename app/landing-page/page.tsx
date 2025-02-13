0bae50feb9f038fdfa10359da0a1e84a2a6b3513
iamqazi <qaziobaid7897@gmail.com>
2025-02-13 20:18:06 +0500

    lint fixes

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -14,32 +14,32 @@ import BannerAnimation from "../components/banner-slider";
 import Image from "next/image";
 
 export default function LandingPage() {
-  // const [loading, setLoading] = useState(true);
-  // const [gifLoaded, setGifLoaded] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [gifLoaded, setGifLoaded] = useState(false);
 
-  // useEffect(() => {
-  //   // Ensure the GIF has enough time to load (9 seconds)
-  //   const timer = setTimeout(() => {
-  //     if (gifLoaded) setLoading(false);
-  //   }, 10000);
+  useEffect(() => {
+    // Ensure the GIF has enough time to load (9 seconds)
+    const timer = setTimeout(() => {
+      if (gifLoaded) setLoading(false);
+    }, 10000);
 
-  //   return () => clearTimeout(timer);
-  // }, [gifLoaded]);
+    return () => clearTimeout(timer);
+  }, [gifLoaded]);
 
-  // if (loading) {
-  //   return (
-  //     <div className="flex items-center justify-center h-screen">
-  //       <Image
-  //         src={"/optimize.gif"}
-  //         alt="Loading..."
-  //         width={500}
-  //         height={500}
-  //         priority
-  //         onLoadingComplete={() => setGifLoaded(true)}
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Image
+          src={"/optimize.gif"}
+          alt="Loading..."
+          width={500}
+          height={500}
+          priority
+          onLoadingComplete={() => setGifLoaded(true)}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
