81206b07d1d8fc68e26fbab495d11369d92b7ca8
iamqazi <qaziobaid7897@gmail.com>
2025-02-23 13:11:12 +0500

    changes

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import FutureOfAi from "../future-of-ai";
 import About from "../about";
 import FAQSection from "../faq";
@@ -10,37 +9,10 @@ import Navbar from "../shared/navbar";
 import Tokenomics from "../tokenomics";
 import BuiltOn from "../built-on";
 import BannerAnimation from "../banner-slider";
-import Image from "next/image";
 import MarqueeBanner from "../banner";
 import Roadmap from "../roadmap";
 
 export default function LandingPage() {
-  const [loading, setLoading] = useState(true);
-  const [gifLoaded, setGifLoaded] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (gifLoaded) setLoading(false);
-    }, 900);
-
-    return () => clearTimeout(timer);
-  }, [gifLoaded]);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Image
-          src={"/optimize.gif"}
-          alt="Loading..."
-          width={500}
-          height={500}
-          priority
-          onLoadingComplete={() => setGifLoaded(true)}
-        />
-      </div>
-    );
-  }
-
   return (
     <div>
       <section className="">
